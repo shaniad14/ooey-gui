89ff54222575df6f51530b7af333fa2fef5ceb30
shaniad14 <shaniad1414@gmail.com>
2026-05-03 12:26:37 -0700

    Added the checkbox

--- main.py
+++ main.py
@@ -23,3 +23,17 @@ def calc():
         bmi = m / (h**2)
     else:
         bmi = 703 * m / (h**2)
+
+         # Display BMI result
+    label_result.config(text="BMI: " + str(round(bmi, 2)))
+
+    # Show category if checkbox is selected
+    if show.get():
+        if bmi < 18.5:
+            label_status.config(text="Underweight")
+        elif bmi < 25:
+            label_status.config(text="Healthy")
+        else:
+            label_status.config(text="Overweight")
+    else:
+        label_status.config(text="")
